cada5a6cf8e1b9af42909b43319552310aaa44d0
D0ct0rDave <dementedworld@gmail.com>
2025-02-12 23:33:36 +0100

    update openal submodule

--- shared/src/FuetEngineFramework/FuetEngine/src/version.h
+++ shared/src/FuetEngineFramework/FuetEngine/src/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 1;
-	static const long BUILD = 1273;
-	static const long REVISION = 6977;
+	static const long BUILD = 1275;
+	static const long REVISION = 6991;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1273;
-	#define RC_FILEVERSION 1,1,1273,6977
-	#define RC_FILEVERSION_STRING "1, 1, 1273, 6977\0"
-	static const char FULLVERSION_STRING[] = "1.1.1273.6977";
+	static const long BUILDS_COUNT = 1275;
+	#define RC_FILEVERSION 1,1,1275,6991
+	#define RC_FILEVERSION_STRING "1, 1, 1275, 6991\0"
+	static const char FULLVERSION_STRING[] = "1.1.1275.6991";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 73;
+	static const long BUILD_HISTORY = 75;
 	
 
 }
